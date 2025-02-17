320e0039b2a510d75d287ab8f306805f87d432e7
Srishu Chintakindi <srishuchintakindi272@gmail.com>
2025-02-17 15:42:50 +0530

    Added the edit task functionality

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:task_management_app/provider/taskProvider.dart';
 import 'package:task_management_app/screens/add_task_screen.dart';
+import 'package:task_management_app/screens/edit_task_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -51,30 +52,35 @@ class _HomeScreenState extends State<HomeScreen> {
                         final task = taskProvider.tasks[index];
                         return Padding(
                           padding: const EdgeInsets.all(10),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.blueGrey.withOpacity(0.7),
-                              borderRadius: BorderRadius.circular(10)
-                            ),
-                            child: ListTile(
-                              title: Text(task.title,style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold,overflow: TextOverflow.ellipsis),),
-                              subtitle: Text(task.description,style: TextStyle(fontWeight: FontWeight.w700,fontSize: 18,overflow: TextOverflow.fade),),
-                              leading: Checkbox(
-                                activeColor: Colors.white.withOpacity(0.6),
-                                  checkColor: Colors.blueGrey,
-                                  value: task.isCompleted,
-                                  onChanged: (value) {
-                                    task.isCompleted = value!;
-                                    taskProvider.updateTask(task);
-                                  }),
-                              trailing: IconButton(
-                                  onPressed: () {
-                                    taskProvider.deleteTask(task.id!);
-                                  },
-                                  icon: Icon(
-                                    Icons.delete,
-                                    color: Colors.red.withOpacity(0.6),
-                                  )),
+                          child: InkWell(
+                            onTap: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=> EditTaskScreen(task: task)));
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.blueGrey.withOpacity(0.7),
+                                borderRadius: BorderRadius.circular(10)
+                              ),
+                              child: ListTile(
+                                title: Text(task.title,style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold,overflow: TextOverflow.ellipsis),),
+                                subtitle: Text(task.description,style: TextStyle(fontWeight: FontWeight.w700,fontSize: 18,overflow: TextOverflow.fade),),
+                                leading: Checkbox(
+                                  activeColor: Colors.white.withOpacity(0.6),
+                                    checkColor: Colors.blueGrey,
+                                    value: task.isCompleted,
+                                    onChanged: (value) {
+                                      task.isCompleted = value!;
+                                      taskProvider.updateTask(task);
+                                    }),
+                                trailing: IconButton(
+                                    onPressed: () {
+                                      taskProvider.deleteTask(task.id!);
+                                    },
+                                    icon: Icon(
+                                      Icons.delete,
+                                      color: Colors.red.withOpacity(0.6),
+                                    )),
+                              ),
                             ),
                           ),
                         );
